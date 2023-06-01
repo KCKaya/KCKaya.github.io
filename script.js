ed57de3a894659985712b3aa7174c6ab8a84ebe9
KCKaya <134875603+KCKaya@users.noreply.github.com>
2023-05-31 23:28:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,17 @@
       {
         "title": "The Future Of Medicine Is Tokens (2023 Article by Rapolas)",
         "slug": "the-token-post",
-        "content": "..about DeSci market and stack"
-        "link": "https://zeeprime.capital/the-future-of-medicine-is-tokens"
+        "content": "..about DeSci market and stack <https://zeeprime.capital/the-future-of-medicine-is-tokens>"
       },
       {
         "title": "DeSci: How Can DAOs Facilitate Collaboration And Push The Open Science Movement Forward (2022 Article by E Couvat)",
         "slug": "the-dao-post",
-        "content": "..about how DeSci can revolutionize the niche of research"
-        "link": "https://coinvise.mirror.xyz/d_jTD1q4pFNXT8JwWUAp0o9TUacsBWNHr_LnRvTmivc"
+        "content": "..about how DeSci can revolutionize the niche of research <https://coinvise.mirror.xyz/d_jTD1q4pFNXT8JwWUAp0o9TUacsBWNHr_LnRvTmivc>"
       },
       {
         "title": "The Future Of Medicine Is Tokens (2022 Article by Ethereum Foundation)",
         "slug": "the-ether-post",
-        "content": "..about the details of the open science market that will be possible by actualizling DeSci"
-        "link": "https://ethereum.org/en/desci/"
+        "content": "..about the details of the open science market that will be possible by actualizling DeSci <https://ethereum.org/en/desci/>"
       },
       
     ]
